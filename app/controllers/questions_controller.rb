f62838559a94a0950cebd1e1283d3c8a58699be6
Evgeniy <Vologzhanin2018@gmail.com>
2023-04-24 16:54:39 +0500

    Add pg and general upgrade

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: %i[update destroy show edit]
+  before_action :set_question, only: %i[update destroy show edit hide]
 
   def create
     question = Question.create(question_params)
@@ -23,6 +23,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
@@ -34,7 +35,7 @@ class QuestionsController < ApplicationController
   end
 
   def hide
-    debugger
+    @question[:hidden] = true
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  put '/questions/:id/hide', to: 'questions#hide'
   resources :questions
 end
