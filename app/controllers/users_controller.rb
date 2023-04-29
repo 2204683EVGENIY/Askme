903e5473c0f7e55b577371a6c41df10ad0e7b5a0
Evgeniy <Vologzhanin2018@gmail.com>
2023-04-29 10:51:55 +0500

    Add model user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email)
-
-    @user = User.create(user_params)
-
+    @user = User.new(user_params)
+    @user.save
+    session[:user_id] = @user.id
     redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password
+    )
   end
 end
