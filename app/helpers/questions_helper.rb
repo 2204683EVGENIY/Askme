79b0e87bf9bcd19491a1451dcba53cc9b0f358bf
Evgeniy <Vologzhanin2018@gmail.com>
2023-05-06 12:07:13 +0500

    refactoring helper question_author

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,7 @@
 module QuestionsHelper
   def question_author(question)
-    if question.author == current_user && question.author.present?
-      "Aвтор - Вы"
-    elsif question.author.present?
-      link_to "@#{question.author.nickname}", user_path(question.author_id)
-    else
-      "Таинственная персона"
-    end
+    return "Aвтор - Вы" if question.author == current_user && question.author.present?
+    return link_to "@#{question.author.nickname}", user_path(question.author_id) if question.author.present?
+    return "Таинственная персона"
   end
 end
