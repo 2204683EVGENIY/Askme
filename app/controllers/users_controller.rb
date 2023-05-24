0cb8c60d6c103c95300bac2bac5a898403e2c47f
Evgeniy <Vologzhanin2018@gmail.com>
2023-05-24 14:51:59 +0500

    rollback fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update destroy]
+  before_action :set_user, only: %i[show edit update destroy]
   before_action :authorize_user, only: %i[edit update destroy]
 
   def create
